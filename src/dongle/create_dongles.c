ebadda165b0868854418e5a6a0fcd846a847b8f2
69Nesta <petitromeo74@gmail.com>
2026-03-19 18:22:58 +0100

    feat: adding protection of mutex failing in dongles creation

--- src/dongle/create_dongles.c
+++ src/dongle/create_dongles.c
@@ -10,15 +10,21 @@ int	create_dongles(t_sim *sim)
 	sim->dongles = malloc(sizeof(t_dongle) * sim->number_of_coders);
 	if (!sim->dongles)
 		return (0);
-
 	index = 0;
 	while (index < sim->number_of_coders)
 	{
 		sim->dongles[index].id = index;
 		sim->dongles[index].taken = FALSE;
 		sim->dongles[index].last_use_time = -1;
-		pthread_mutex_init(&sim->dongles[index].mutex, NULL);
+		if (pthread_mutex_init(&sim->dongles[index].mutex, NULL))
+			break;
 		index++;
 	}
+	while(index != sim->number_of_coders && index > 0)
+	{
+		pthread_mutex_destroy(&sim->dongles[--index].mutex);
+	}
+	if (index <= 0)
+		return (0);
 	return (1);
 }
